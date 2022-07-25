1fbc6565c7002f07436a37315bcc50421e451f94
Michael B <michaelbarr24@hotmail.com>
2022-07-25 22:34:54 +0100

    Add basic backend

--- moths-frontend/src/components/Today/Today.js
+++ moths-frontend/src/components/Today/Today.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./Today.scss";
 
 function Today() {
-  return <div className="today container">Today</div>;
+  return (
+    <div className="today container">
+      <h2>Check out what's been found in the last 24 hours!</h2>
+    </div>
+  );
 }
 
 export default Today;
