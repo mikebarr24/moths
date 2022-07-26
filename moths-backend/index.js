1b9422600277044a0933d84ab7de2288bb49f77b
Michael B <michaelbarr24@hotmail.com>
2022-07-26 08:58:34 +0100

    Add docker compose

--- moths-backend/index.js
+++ moths-backend/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
-
 const port = process.env.PORT || 3001;
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
 const server = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
